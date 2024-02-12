0c59c6a2715be7620fcfacb377b5a39d36958b81
DigitalRedPanda <osama-12341905@hotmail.com>
2024-02-12 22:15:44 +0300

    #2 fix with other bug fixes and refinements

--- src/bot.nim
+++ src/bot.nim
@@ -1,14 +1,16 @@
-# from env import loadEnv
-# from tables import `[]`
-import std/terminal
+from env import loadEnv
+from tables import `[]`
+
+import std/[terminal], strformat, os, client, net
+
+
 
 proc main() =
-    # let
-    #     info = loadEnv("src/.env")
-    #     client = newClient(info["TOKEN"], info["USER_ID"], "digital_red_panda")
-    # client.connectToChat()
-    # client.joinChannel("digital_red_panda")
-    # client.on(MessageEvent, 10)
-    stdout.styledWriteLine({styleBlink}, "lmao")
-    discard
+    let 
+        info = loadEnv("src/.env")
+        client = newClient(info["TOKEN"], info["USER_ID"], "digital_red_panda") 
+    client.connectToChat()
+    client.on(MessageEvent, 10, proc(channel, user, message: string) = echo("[\e[1m", channel, "\e[0m] [\e[1m", user, "\e[0m]: ", message))
+    #[ client.on(MessageEvent, 10, proc(channel, user, message: string) = stdout.writeLine(fmt"['\e[1m'{channel}'\e[0m'] ['\e[1m'{user}'\e[0m']: {message}")) ]#
+
 main()
--- src/client.nim
+++ src/client.nim
@@ -1,7 +1,7 @@
 from net import Socket, newSocket, send, recvLine, connect, Port
 from os import sleep
 from strutils import isEmptyOrWhitespace, split
-from re import re, replace
+import re
 
 type Client* = object
     token*, id*, displayName*: string
@@ -11,42 +11,48 @@ type Event* = enum
     MessageEvent,
     CommandEvent,
 
-const pattern = re"(^([:!]\w+){2}|\.tmi\.twitch\.tv)"
+let 
+    messagePattern = re"(^([:!]\w+){2}|\.tmi\.twitch\.tv)"
+    pattern = re"^:tmi\.twitch\.tv \d{3}"
 
 template loop(body: untyped): void = 
     while true:
         body
-proc sendMsg(socket: Socket, msg: string) = socket.send(msg & "\c\L")
+proc sendMsg*(socket: Socket, msg: string) = socket.send(msg & "\r\n")
 
 func newClient*(userToken, userId, displayedName: string, userSocket = newSocket()): Client =
     return Client(token: userToken, id: userId, displayName: displayedName, socket: userSocket)
 
+proc joinChannel*(client: Client, channel: string) =
+    client.socket.sendMsg("JOIN #" & channel)
+
 proc connectToChat*(client: Client) = 
     client.socket.connect("irc.chat.twitch.tv", Port(6667))
     client.socket.sendMsg("PASS oauth:" & client.token & "\nNICK " & client.displayName)
+    client.joinChannel(client.displayName)
 
-proc joinChannel*(client: Client, channel: string) =
-    client.socket.sendMsg("JOIN #" & channel)
-   
 proc sendChatMessage*(client: Client, channelName, message: string) = 
     client.socket.sendMsg("PRIVMSG #" & channelName & " :" & message)
 
-proc on*(client: Client, event: Event, timeout: int, exec: proc(channel, user, message: string): void) = 
+proc on*(client: Client, event: Event, timeout: int, exec: proc(channel, user, message: string): void) =  
     loop:
-        let message = client.socket.recvLine().replace(re)
-        echo message
-        if not message.isEmptyOrWhitespace:
-            let
-                messageFeed = message.split(":") 
-                identifiers = messageFeed[0].split(" ") 
-            if identifiers[0] == "PING":
-                client.socket.sendMsg("PONG :tmi.twitch.tv")
-            else:
-                case event:
-                    of MessageEvent:
-                      if messageFeed[1][0] != '!':
-                        exec(channel=identifiers[2], user=identifiers[0], message=messageFeed[1])
-                    of CommandEvent:
-                      if messageFeed[1][0] == '!':
-                        exec(channel=identifiers[2], user=identifiers[0], message=messageFeed[1])
+        let message = client.socket.recvLine().replace(messagePattern)
+        try:
+            if not (message.isEmptyOrWhitespace or message.match(pattern)):
+                let identifiers = message.split(" ") 
+                if identifiers[0] == "PING":
+                    client.socket.sendMsg("PONG :tmi.twitch.tv")
+                elif identifiers[1] == "JOIN":
+                    stdout.writeLine "joined \e[1m" & identifiers[2] & "\e[0m"
+                else:
+                    let messageFeed = message.split(":") 
+                    case event:
+                        of MessageEvent:
+                          if messageFeed[1][0] != '!':
+                            exec(channel=identifiers[2], user=identifiers[0], message=messageFeed[1])
+                        of CommandEvent:
+                          if messageFeed[1][0] == '!':
+                            exec(channel=identifiers[2], user=identifiers[0], message=messageFeed[1])
+        except:
+            stdout.writeLine("[\e[1;31mERROR\e[0m] message `\e[1m" & message & "\e[0m` have raised `\e[1m" & getCurrentExceptionMsg() & "\e[0m`")
         sleep(timeout)
